c58feae49b63ee95e150fffdaedba5d1c72817b8
tgcoding <godfrey.tom.1@gmail.com>
2020-12-13 17:44:26 -0500

    Add spring security to open up CORS (#2)

---
+++ src/main/java/com/tgcoding/mealscalendar/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.tgcoding.mealscalendar.config;
+
+import com.tgcoding.mealscalendar.security.RestAuthenticationEntryPoint;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                    .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .csrf()
+                    .disable()
+                .formLogin()
+                    .disable()
+                .httpBasic()
+                    .disable()
+                .exceptionHandling()
+                    .authenticationEntryPoint(new RestAuthenticationEntryPoint())
+                    .and()
+                .authorizeRequests()
+                    .antMatchers("/",
+                        "/error",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
+                        .permitAll()
+                    .antMatchers("/auth/**", "/oauth2/**", "/ready", "/ready/")
+                        .permitAll()
+                    .anyRequest()
+                        .permitAll();
+    }
+}
